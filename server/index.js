16ae700650a7330a77a6af5e7e0ed34c403b76c9
Shreesh Kulkarni <sgkul2000@gmail.com>
2020-04-30 08:00:50 +0530

    mongo fix

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
+require('dotenv').config({ path: __dirname + '/.env' });
 
 //middleware
 app.use(bodyParser.json());
